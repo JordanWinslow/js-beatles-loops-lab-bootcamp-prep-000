a6da1c7566c7b284c62f9c57606a559c7d2cfd9b
Jordan Winslow <admin@jordanwinslow.me>
2019-07-03 21:40:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,3 +18,10 @@ function johnLennonFacts(facts){
   }
   return facts
 }
+function iLoveTheBeatles(number){
+  let newArray = []
+  do {
+    newArray.push("I love the Beatles!")
+    number++
+  } while (number < 15)
+}
